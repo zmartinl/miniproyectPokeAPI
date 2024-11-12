b308f85ce3ef427ecfcf943c50e5b38597f8a5d2
albdom222 <33178106+albdom222@users.noreply.github.com>
2024-11-12 12:47:17 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Ejercicio 1: Buscar y mostrar información del Pokemon con FETCH
+ */
+/**
+ * Función para buscar pokemon ejercicio 1 y 2.
+ * 
+*/
+
+async function buscarPokemon(){
+    /**
+     * Tu código aquí.
+     */
+
+
+
+}
+
+/**
+ *  Descomentar para hacer uso de la función.
+ */
+//document.getElementById('search-btn').addEventListener('click',buscarPokemon);
+
+
+/**
+ * Ejercicio 3: buscar pokemon con JQuery AJAX.
+ */
+
+function buscarPokemonJQueryAJAX(){
+    /**
+     * Tu código aquí.
+     */
+}
+
+/**
+ * Haciendo uso de JQuery, descomentar para usar la función buscarPokemonJQueryAJAX
+*/
+/**
+$(document).ready(function(){
+    $('#search-btn').on('click', buscarPokemonJQueryAJAX);
+}); 
+*/
